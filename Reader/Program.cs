9fd5b2b27310eb28b6a923d7bb43d2a0027072af
ramamus <satishramamurthi@hotmail.com>
2015-12-03 13:37:55 -0600

    Send Recive

---
+++ Reader/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reader
+{
+    class Program
+    {
+        static string connectionString = "HostName=mtclab.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=27pCqd1J/UQmfm0RpfrvR8aXdhtSz5pQMLFylc8RHJo=";
+        static string iotEndPoint = "messages/events";
+        static EventHubClient eventHubClient;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Receiving Messages\n");
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, iotEndPoint);
+            var partitions = eventHubClient.GetRuntimeInformation().PartitionIds;
+
+            foreach (var partition in partitions)
+            {
+                ReceiveMessages(partition);
+            }
+            Console.ReadKey();
+        }
+
+        private async static Task ReceiveMessages(string partition)
+        {
+            var eventHubReceiver = eventHubClient.GetDefaultConsumerGroup().CreateReceiver(partition, DateTime.Now);
+            while(true)
+            {
+                var eventData = await eventHubReceiver.ReceiveAsync();
+                if (eventData == null) continue;
+                var data = Encoding.UTF8.GetString(eventData.GetBytes());
+                Console.WriteLine("Message rcvd: {0} Data {1}", partition, data);
+            }
+        }
+    }
+}
